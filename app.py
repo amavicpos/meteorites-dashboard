396a7b3d7e3ee8ec480619ddb3bf26b7b0707b36
amavicpos <100805638+amavicpos@users.noreply.github.com>
2023-09-13 19:05:15 +0100

    Create basic map

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from dash import Dash
+from dash import Dash, html, dcc
+import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv()
+df = pd.read_json('meteorites.json')
 
 app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H1("NASA meteorite strike dashboard", style={'text-align': 'center'}),
+    dcc.Graph(
+        id='meteorite-map',
+        figure=px.scatter_geo(
+            df,
+            lon='reclong',
+            lat='reclat',
+            hover_name='name',
+            projection="natural earth"
+        ),
+    ),
+])
 
 if __name__ == '__main__':
     app.run(debug=True)
